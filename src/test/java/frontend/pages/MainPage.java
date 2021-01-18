8abfad458e3cc6f131a25bd717d67bcfdd31cb87
Bartosz Szerlag <bartosz.szerlag@allegro.pl>
2021-01-18 17:42:33 +0100

    logger

--- src/test/java/frontend/pages/MainPage.java
+++ src/test/java/frontend/pages/MainPage.java
@@ -3,15 +3,20 @@ package frontend.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 import static frontend.config.DriverManager.getDriver;
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openqa.selenium.support.PageFactory.initElements;
 
 public class MainPage {
+
+    private final Logger logger = LoggerFactory.getLogger(MainPage.class);
 
     public MainPage() {
         initElements(getDriver(), this);
@@ -37,49 +42,58 @@ public class MainPage {
 
     public MainPage typeIntoTitleInput(String title) {
         titleInput.sendKeys(title);
+        logger.info(format("### User typed title: %s ###", title));
         return this;
     }
 
     public MainPage typeIntoPlatformInput(String platform) {
         platformInput.sendKeys(platform);
+        logger.info(format("### User typed platform: %s ###", platform));
         return this;
     }
 
     public MainPage typeIntoIdInput(String id){
         idInput.sendKeys(id);
+        logger.info(format("### User typed id: %s ###", id));
         return this;
     }
 
     public MainPage clickAddButton() {
         addButton.click();
+        logger.info("### User clicked Add button ###");
         return this;
     }
 
     public MainPage clickReloadPageButton() {
         reloadPageButton.click();
+        logger.info("### User clicked Reload Page button ###");
         return this;
     }
 
     public MainPage clickRemoveButton() {
         removeButton.click();
+        logger.info("### User clicked Remove button ###");
         return this;
     }
 
     public MainPage assertThatButtonAlertContainsMessage(String message) {
         WebElement buttonAlert = getDriver().findElement(By.className("series-alert"));
-        assertTrue(buttonAlert.getText().contains(message), message + " is not expected message");
+        assertTrue(buttonAlert.getText().contains(message), format("%s is not expected message", message));
+        logger.info(format("### SUCCESS: button alert contains proper text: %s ###", message));
         return this;
     }
 
     public MainPage assertThatAddedSeriesIsOnList(String title) {
         WebElement firstItem = getDriver().findElements(By.className("list-group-item")).get(0);
-        assertTrue(firstItem.getText().contains(title), title + " is not expected title");
+        assertTrue(firstItem.getText().contains(title), format("%s is not expected title", title));
+        logger.info(format("### SUCCESS: list contains TV series: %s ###", title));
         return this;
     }
 
     public MainPage assertThatSeriesListIsEmpty() {
         List<WebElement> list = getDriver().findElements(By.className("list-group-item"));
         assertEquals(0, list.size(), "List is not empty");
+        logger.info("### SUCCESS: list is empty ###");
         return this;
     }
 }
--- src/test/java/frontend/tests/TestBase.java
+++ src/test/java/frontend/tests/TestBase.java
@@ -3,15 +3,27 @@ package frontend.tests;
 import backend.services.FavoriteSeriesService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static frontend.config.DriverConfig.initDriverConfig;
 import static frontend.config.DriverConfig.navigateToPage;
 import static frontend.config.DriverManager.closeDriver;
 import static frontend.config.PropertiesReader.getProperties;
+import static java.lang.String.format;
 
 public class TestBase {
 
+    private final Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     static FavoriteSeriesService favoriteSeriesService;
+
+    @BeforeEach
+    void displayTestName(TestInfo testInfo) {
+        logger.info(format("### TEST: %s ###", testInfo.getDisplayName()));
+    }
 
     @BeforeAll
     static void setup() {
